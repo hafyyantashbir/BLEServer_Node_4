42f03772bd2bca3b8c9de226cf2e3e7fc78ccf7a
hafyyantashbir <hafyyantashbir@gmail.com>
2024-02-17 18:22:09 +0700

    dual loop
    pembacaan sensor dipindahkan ke loop 2 sehingga pembacaan sensor terus berjalan dan tidak terpengaruh pada loop 1 yang menjalankan pengiriman data menggunakan NRF

--- src/BLEServer_Node_4.cpp
+++ src/BLEServer_Node_4.cpp
@@ -82,7 +82,7 @@ void setID() {
 }
 
 //konfigurasi stack size
-SET_LOOP_TASK_STACK_SIZE(64*1024); // 64KB
+SET_LOOP_TASK_STACK_SIZE(32*1024); // 64KB
 
 //konfigurasi RTC
 RTC_DS3231 rtc;
@@ -99,31 +99,29 @@ uint8_t dataBuffer[MAX_PAYLOAD_SIZE];  //MAX_PAYLOAD_SIZE is defined in RF24Netw
 
 //variabel DATA
 int node_asal = 4;
-unsigned long TofX = 5; // data TofX
-unsigned long TofY = 10; // data TofY
-unsigned long TofZ = 15; // data TofZ
+unsigned long TofX; // data TofX
+unsigned long TofY; // data TofY
+unsigned long TofZ; // data TofZ
 String datakirim;
 
 //variabel millis
 unsigned long currentMillis = 0;
 
 //Fungsi untuk 2 loop
-//TaskHandle_t Task1;
+TaskHandle_t Task1;
 
 //program loop 2
-//void loop2( void * parameter) {
-//  for (;;) {
-//    unsigned long currentTime = millis(); // Waktu saat ini
-//
-//    if (currentTime - previousTime >= intervalmillis) {
-//      previousTime = currentTime; // Perbarui waktu sebelumnya
-//      Serial.println("MODE : SCANNING......");
-//      BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-//      Serial.print("RSSI NODE 1 : " + String(NODE_1_RSSI));
-//      Serial.println(" || RSSI NODE 2 : " + String(NODE_2_RSSI));
-//    }
-//  }
-//}
+void bacasensor( void * parameter) {
+ for (;;) {
+    lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
+    lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
+    lox3.rangingTest(&measure3, false); // pass in 'true' to get debug data printout!
+    TofX = measure1.RangeMilliMeter;
+    TofY = measure2.RangeMilliMeter;
+    TofZ = measure3.RangeMilliMeter;
+    Serial.println("Running on Core : "+String(xPortGetCoreID())+", Tof X : "+String(TofX)+", Tof Y : "+String(TofY)+", Tof Z : "+String(TofZ));
+ }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -143,16 +141,6 @@ void setup() {
   
   Serial.println(F("Starting..."));
   setID();
-
-  //Fungsi untuk 2 loop
-  //  xTaskCreatePinnedToCore(
-  //    loop2,
-  //    "BLE_SCANNING",
-  //    1000,
-  //    NULL,
-  //    1,
-  //    &Task1,
-  //    0);
 
   if (! rtc.begin()) {
     Serial.println("Tidak dapat menemukan RTC! Periksa sirkuit.");
@@ -181,6 +169,16 @@ void setup() {
   printf_begin();
   radio.printDetails();  // print detail konfigurasi NRF24L01
 
+  //Fungsi untuk 2 loop
+   xTaskCreatePinnedToCore(
+     bacasensor,    /* Task function. */
+     "baca_sensor", /* name of task. */
+     32768,         /* Stack size of task */
+     NULL,          /* parameter of the task */
+     1,             /* priority of the task */
+     &Task1,        /* Task handle to keep track of created task */
+     0);            /* pin task to core 0 */
+
   // print memori stack keseluruhan
   Serial.printf("Arduino Stack was set to %d bytes", getArduinoLoopTaskStackSize());
   // print sisa memori stack pada void setup
@@ -196,14 +194,8 @@ void loop() {
   StaticJsonDocument<128> doc;
 
   // Mengirim data ke master
-  if (millis() - currentMillis >= 1000) {
+  if (millis() - currentMillis >= 250) {
     currentMillis = millis();
-    lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
-    lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
-    lox3.rangingTest(&measure3, false); // pass in 'true' to get debug data printout!
-    TofX = measure1.RangeMilliMeter;
-    TofY = measure2.RangeMilliMeter;
-    TofZ = measure3.RangeMilliMeter;
     doc["NodeID"] = String(node_asal);
     doc["TofX"] = String(TofX);
     doc["TofY"] = String(TofY);
